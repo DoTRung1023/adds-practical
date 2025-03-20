435abe5a09ed085bd192a02d893bbeb412ad69d5
DoTRung1023 <a1899443@adelaide.edu.au>
2025-03-20 18:47:24 +1030

    .

--- main.cpp
+++ main.cpp
@@ -6,14 +6,29 @@ using namespace std;
 
 int main(){
     Truckloads t;
-    int numCrates = 1024;
-    int loadSize = 5;
+    // test normal case
+    // int numCrates = 1024;
+    // int loadSize = 5;
+
+    // int numCrates = 14;
+    // int loadSize = 3;
+
+    //test boundary case: 2<=numCrates<=10000 && 1<=loadSize<=numCrates-1
+    int numCrates = 10000;
+    int loadSize = 9999;
+
+    // int numCrates = 2;
+    // int loadSize = 1;
+
     cout << t.numTrucks(numCrates, loadSize) << endl;
 
     Reverser s;
+    // test normal number and string
     cout << s.reverseDigit(10636384) << endl;
     cout << s.reverseString("do hai trung") << endl;
-
+    // test negative number and special characters in string
+    cout << s.reverseDigit(-6734) << endl;
+    cout << s.reverseString(";.]/;,;][]") << endl;
 
     return 0;
 }
--- makefile
+++ makefile
@@ -10,5 +10,9 @@ test3: test3.cpp EfficientTruckloads.cpp
 	@g++ -std=c++11 test3.cpp EfficientTruckloads.cpp -o test3
 	@./test3
 
+main: main.cpp Reverser.cpp Truckloads.cpp
+	@g++ -std=c++11 main.cpp Reverser.cpp Truckloads.cpp -o main
+	@./main
+
 clear:
-	@rm -f test1 test2 test3
+	@rm -f test1 test2 test3 main
